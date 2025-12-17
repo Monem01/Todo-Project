3568051d70abba9a44ed571879306eb22ced3ca9
Monem01 <khalloulimonem32@gmail.com>
2025-12-17 15:33:33 +0100

    aaaaaaaaaaaaaaaaaaaaaaaa

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST, // mp2l-postgres.postgres.database.azure.com
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME, // peandb
+  user: process.env.DB_USER, // postgres@mp2l-postgres
+  password: process.env.DB_PASSWORD, // Mot de passe exact
   dialect: "postgres",
-  ssl: { require: true, rejectUnauthorized: false },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: 5,
     min: 0,
